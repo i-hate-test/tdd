5a17d150edb47159679319de9e0549cbd4507595
han <bjh4135@gmail.com>
2024-02-27 11:05:26 +0900

    Chapter 4~6

---
+++ han/src/main/java/chap06/FileExecutor.java
@@ -0,0 +1,12 @@
+package chap06;
+
+import java.io.File;
+
+public class FileExecutor {
+  public void execute(File file) {
+    if (!file.exists()) {
+      throw new IllegalArgumentException("File not found: " + file.getPath());
+    }
+    return;
+  }
+}
